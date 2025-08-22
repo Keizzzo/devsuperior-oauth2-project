f817848acfeeb7e877e850dac2e2cf3c65eae48a
Keizo Kobayashi <keizo.kobayashi@outlook.com.br>
2025-08-21 21:49:24 -0300

    Adding Spring Security into the project class

---
+++ src/main/java/com/devsuperior/demo/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.devsuperior.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // disable protection against csrf attacks to steal data in session
+        // REST request does not store data in session, so safe game :)
+        http.csrf(csrf -> csrf.disable());
+
+        // setup permissions for request
+        // specific access restriction will be defined in route access management
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
+}
